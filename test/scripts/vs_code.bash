ed5e79c319589c4e394a4d079f1196e1423ab425
rostest <hhoss@gmx.de>
2021-06-24 13:49:44 +0200

    initial version based on sick_scan
    initial version based on https://github.com/SICKAG/sick_scan commit 5287911 on Jun 24, 2021 (sick_scan_pretest release 1.10.11)

---
+++ test/scripts/vs_code.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+gedit ./make.bash ./run_simu_tim7xx_tim7xxS.bash ./run_simu_lms5xx.bash ./run_simu_lms1xx.bash & 
+
+pushd ../../../..
+source /opt/ros/melodic/setup.bash
+source ./install/setup.bash
+
+code ./sick_scan_xd_vscode.code-workspace
+popd 
