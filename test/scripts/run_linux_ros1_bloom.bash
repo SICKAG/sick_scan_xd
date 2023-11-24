4b02a444bb79d32a104259f58113cb6ec8fc958c
rostest <ros.test@lehning.de>
2023-11-24 10:06:56 +0100

    bloom pretest with package version 3.1

---
+++ test/scripts/run_linux_ros1_bloom.bash
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+printf "\033c"
+source /opt/ros/noetic/setup.bash
+rm -rf /tmp/prerelease_job
+mkdir -p /tmp/prerelease_job
+pushd /tmp/prerelease_job
+
+generate_prerelease_script.py \
+          https://raw.githubusercontent.com/ros-infrastructure/ros_buildfarm_config/production/index.yaml \
+          noetic default ubuntu focal amd64 \
+          --custom-repo \
+            sick_scan_xd:git:https://github.com/SICKAG/sick_scan_xd:master \
+            libsick_ldmrs:git:https://github.com/SICKAG/libsick_ldmrs:master \
+          --level 1 \
+          --output-dir ./        
+
+rm -rf ~/.ccache ; mkdir -p ~/.ccache ; ./prerelease.sh
+popd
+
