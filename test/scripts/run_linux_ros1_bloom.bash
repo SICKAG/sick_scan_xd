c884deb03cd806c622922b3fdd2b58b20c38ae71
rostest <ros.test@lehning.de>
2023-12-01 12:15:29 +0100

    build options for bloom support

--- test/scripts/run_linux_ros1_bloom.bash
+++ test/scripts/run_linux_ros1_bloom.bash
@@ -9,11 +9,12 @@ pushd /tmp/prerelease_job
 generate_prerelease_script.py \
           https://raw.githubusercontent.com/ros-infrastructure/ros_buildfarm_config/production/index.yaml \
           noetic default ubuntu focal amd64 \
-          --custom-repo \
-            sick_scan_xd:git:https://github.com/SICKAG/sick_scan_xd:feature/bloom_pretest \
-            libsick_ldmrs:git:https://github.com/SICKAG/libsick_ldmrs:master \
+          --custom-repo sick_scan_xd:git:https://github.com/SICKAG/sick_scan_xd:feature/bloom_pretest \
           --level 1 \
           --output-dir ./        
+
+# Note: For ldmrs support use option
+# --custom-repo sick_scan_xd:git:https://github.com/SICKAG/sick_scan_xd:feature/bloom_pretest libsick_ldmrs:git:https://github.com/SICKAG/libsick_ldmrs:master
 
 rm -rf ~/.ccache ; mkdir -p ~/.ccache ; ./prerelease.sh
 popd
